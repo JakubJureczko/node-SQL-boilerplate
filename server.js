4504de9fb6ccc894889c29a44895458e6fb5570d
Jakub Jureczko <untech.untech@gmail.com>
2020-08-25 11:22:31 +0200

    adding id

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const app = express();
 const pool = new Pool();
 
 app.get("/", (req, res) => res.send("hello world"));
 
-app.listen('3000', () => console.log('connected'));
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  pool
+    .query(`SELECT * FROM users WHERE id=$1;`, [id])
+    .then((data) => res.json(data.rows))
+    .catch((e) => res.sendStatus(404));
+});
+
+app.listen("3000", () => console.log("connected"));
