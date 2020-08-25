ef75b66a924fe6f79f72f74d644e3f27a3e68ddc
Jakub Jureczko <untech.untech@gmail.com>
2020-08-25 12:43:56 +0200

    s

--- server.js
+++ server.js
@@ -7,12 +7,34 @@ const pool = new Pool();
 
 app.get("/", (req, res) => res.send("hello world"));
 
+app.get("/users", (req, res) => {
+  pool
+    .query('SELECT * FROM users')
+    .then((data) => res.json(data.rows))
+    .catch((e) => res.sendStatus(500));
+});
+
 app.get("/users/:id", (req, res) => {
   const { id } = req.params;
   pool
     .query(`SELECT * FROM users WHERE id=$1;`, [id])
     .then((data) => res.json(data.rows))
-    .catch((e) => res.sendStatus(404));
+    .catch((e) => res.sendStatus(500));
+});
+
+app.get("/orders", (req, res) => {
+  pool
+    .query('SELECT * FROM orders')
+    .then((data) => res.json(data.rows))
+    .catch((e) => res.sendStatus(500));
+});
+
+app.get("/orders/:id", (req, res) => {
+  const { id } = req.params;
+  pool
+    .query(`SELECT * FROM orders  WHERE id=$1;`, [id])
+    .then((data) => res.json(data.rows))
+    .catch((e) => res.sendStatus(500));
 });
 
 app.listen("3000", () => console.log("connected"));
